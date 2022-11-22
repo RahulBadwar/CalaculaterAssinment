8bef41f0df2cfa2ab44030b773334af08f92e325
rbadwar <rbadwar@IN-G6FD9G3>
2022-11-22 22:28:18 +0530

    Add project files.

---
+++ CalaculaterAssinment/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalaculaterAssinment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Calculater()
+        {
+            return View();
+        }
+
+        public ActionResult Calculater1()
+        {
+            return View();
+        }
+
+        public ActionResult CalculterResult()
+        {
+            int a =Convert.ToInt32 (Request.QueryString["num1"]);
+            int b =Convert.ToInt32 (Request.QueryString["num2"]);
+            double result = 0;
+            if(Request.QueryString["add"]!=null)
+            {
+                result = a + b;
+            }
+            if (Request.QueryString["sub"] != null)
+            {
+                result = a - b;
+            }
+            if(Request.QueryString["mul"] != null)
+            {
+                result = a * b;
+            }
+            if(Request.QueryString["div"] != null)
+            {
+               double result1 = (double)a / (double)b;
+            }
+
+            ViewBag.Result = "Your Output is    " + result;
+            return View();
+        }
+    }
+}
